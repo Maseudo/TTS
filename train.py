f7c86973f5100befae0e03032db8de081112ca6f
Maseudo <46875906+Maseudo@users.noreply.github.com>
2024-03-25 09:02:27 -0500

    f

--- train.py
+++ train.py
@@ -6,11 +6,9 @@ from TTS.tts.configs.shared_configs import BaseDatasetConfig, CharactersConfig
 from TTS.tts.configs.vits_config import VitsConfig
 from TTS.tts.datasets import load_tts_samples
 from TTS.tts.models.vits import Vits, VitsArgs, VitsAudioConfig
-from TTS.tts.models.glow_tts import GlowTTS
 from TTS.tts.utils.speakers import SpeakerManager
 from TTS.tts.utils.text.tokenizer import TTSTokenizer
 from TTS.utils.audio import AudioProcessor
-from TTS.tts.configs.glow_tts_config import GlowTTSConfig
 
 output_path = "./tts/"
 
